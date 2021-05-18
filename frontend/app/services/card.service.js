ebe3d7029e9ff2cff3dc5a9b416a5d63a24ef1bd
Lucas Mendonça <lucas_mendonca@trimble.com>
2021-05-18 13:33:34 -0300

    Removes test method in card.service.js

--- frontend/app/services/card.service.js
+++ frontend/app/services/card.service.js
@@ -3,10 +3,6 @@ class CardService {
         this.$http = $http;
     }
 
-    test() {
-        alert('oiiiii')
-    }
-
     getExistingCards() {
         return this.$http.get('https://jsonplaceholder.typicode.com/users')
             .then((res) => { return res.data }, this.onError);
