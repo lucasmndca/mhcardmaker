660b0a3912493d6dde1456895907f7a077eae1c8
Lucas Mendonça <lucas_mendonca@trimble.com>
2021-05-26 09:37:12 -0300

    Introduces very basic stuff such as the initial structure of the backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
-const config = require('./config.json');
-const express = require('express');
-const app = express();
-const routes = require('./src/routes/routes');
+import config from './src/config/config.json';
+import app from './src/config/app';
+import cors from 'cors';
+import corsConfig from './src/config/cors';
+import cardsRouter from './src/routes/cards';
+import authRouter from './src/routes/auth';
 
-app.use(routes);
+app.use(cors(corsConfig()));
+app.use('/cards', cardsRouter);
+app.use('/auth', authRouter);
 
 app.listen(config.defaultPort, () => console.log(`Server running on port ${config.defaultPort}`));
